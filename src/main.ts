4166a4971d88237b6e69ab601d4605feb582f354
monaaaaaaaaa <ramona@sprenger.ch>
2024-08-15 12:00:02 -0300

    Update Info when enter

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,22 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
-
+    WA.ui.modal.openModal({
+        title: "WorkAdventure website",
+        src: 'https://www.proofofculture.ch/ta-swiss-info',
+        allow: "fullscreen",
+        allowApi: true,
+        position: "center"
+        });
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
 export {};
